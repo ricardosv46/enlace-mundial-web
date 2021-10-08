f8ce3591d0f32502c2973f7719b59716a2b129f7
Jose Jollja <jjollja@ctdevelopers.com>
2021-10-08 15:30:45 -0500

    Compartir en redes sociales desde card

--- components/cards/components/compartir/index.js
+++ components/cards/components/compartir/index.js
@@ -1,27 +1,56 @@
-import react from "react";
+import react, { useState } from "react";
 
 import styles from "./styles.module.scss";
 
 export default function Compartir() {
-  function mostrarContenido(e) {
-    e.stopPropagation();
-    alert("w");
+  const [mostrarContenido, setMostrarContenido] = useState(false);
+
+  function click(e) {
+    e.preventDefault();
+
+    mostrarContenido ? setMostrarContenido(false) : setMostrarContenido(true);
   }
 
   return (
     <>
-      <div className="position-relative">
-      <button
-        type="button"
-        className={`${styles.compartir} text-dark d-flex justify-content-center align-items-center border-0`}
-        onClick={mostrarContenido}
+      <div
+        className="position-relative d-flex"
+        onClick={click}
+        onMouseEnter={() => setMostrarContenido(true)}
+        onMouseLeave={() => setMostrarContenido(false)}
       >
-        <i class="fas fa-share-alt"></i>
-      </button>
-      
-      <section className={`${styles.compartir_contenido}`}>
-          Content
-      </section>
+        <button
+          type="button"
+          className={`${styles.compartir} text-dark d-flex justify-content-center align-items-center border-0`}
+        >
+          <span className="d-inline-block w-100 h-100">
+            <i class="fas fa-share-alt"></i>
+          </span>
+        </button>
+
+        {mostrarContenido ? (
+          <section
+            className={`${styles.compartir_contenido} d-flex align-items-center ml-2 px-2`}
+          >
+            <img
+              src="/imagenes/redes-sociales/facebook.png"
+              alt="Facebook"
+              className={styles.compartir_logo}
+            />
+            <img
+              src="/imagenes/redes-sociales/instagram.png"
+              alt="Instagram"
+              className={`${styles.compartir_logo} mx-2`}
+            />
+            <img
+              src="/imagenes/redes-sociales/whatsapp.png"
+              alt="Whatsapp"
+              className={styles.compartir_logo}
+            />
+          </section>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
