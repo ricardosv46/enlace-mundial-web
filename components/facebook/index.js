1af3c53aaeb79da1cd80233c1277b8a7fcc0228a
Cristian <kazragore96@gmail.com>
2022-02-28 15:53:07 -0500

    fix(facebook_chat): corregido page_id

--- components/facebook/index.js
+++ components/facebook/index.js
@@ -1,39 +1,39 @@
-import React from "react"
-import Script from "next/script"
+import React from "react";
+import Script from "next/script";
+
 const Facebook = () => {
   return (
     <div>
-      <div id='fb-root'></div>
+      <div id="fb-root" />
 
       <div
-        id='fb-customer-chat'
-        className='fb-customerchat'
-        attribution='biz_inbox'
-        page_id='101774235552574'
-      ></div>
+        attribution="biz_inbox"
+        page_id="507210889388752"
+        className="fb-customerchat"
+      />
 
-      <Script strategy='lazyOnload'>
-        {` var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "101774235552574");
-      chatbox.setAttribute("attribution", "biz_inbox");
+      <Script>
+        {`
 
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v11.0'
-        });
-      };
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml            : true,
+              version          : 'v11.0'
+            });
+          };
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'))`}
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'))
+      
+      `}
       </Script>
     </div>
-  )
-}
+  );
+};
 
-export default Facebook
+export default Facebook;
