5e23f9d392fae488e254cfa40cf62641301b4cce
yarmolem <kazragore96@gmail.com>
2022-04-23 11:57:13 -0500

    feat: Agregado context MercadoPago

---
+++ data/mercadoPagoformConfig.js
@@ -0,0 +1,39 @@
+export const mercadoPagoFormConfig = {
+  id: 'form-checkout',
+  cardholderName: {
+    id: 'form-checkout__cardholderName',
+    placeholder: 'Titular de la tarjeta'
+  },
+  cardholderEmail: {
+    id: 'form-checkout__cardholderEmail',
+    placeholder: 'E-mail'
+  },
+  cardNumber: {
+    id: 'form-checkout__cardNumber',
+    placeholder: 'Número de la tarjeta'
+  },
+  cardExpirationDate: {
+    id: 'form-checkout__cardExpirationDate',
+    placeholder: 'Data de vencimiento (MM/YY)'
+  },
+  securityCode: {
+    id: 'form-checkout__securityCode',
+    placeholder: 'Código de seguridad'
+  },
+  installments: {
+    id: 'form-checkout__installments',
+    placeholder: 'Cuotas'
+  },
+  identificationType: {
+    id: 'form-checkout__identificationType',
+    placeholder: 'Tipo de documento'
+  },
+  identificationNumber: {
+    id: 'form-checkout__identificationNumber',
+    placeholder: 'Número de documento'
+  },
+  issuer: {
+    id: 'form-checkout__issuer',
+    placeholder: 'Banco emisor'
+  }
+}
