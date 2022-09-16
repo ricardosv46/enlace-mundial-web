9ca6f17e58ba36b4feb7976317a4f7fd6251d847
jeanflores99 <ajiliof@gmail.com>
2022-09-16 14:25:43 -0500

    comenzando a agregar el swipe al home

---
+++ components/home/banner/ItemSecondary.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ItemSecondary = () => {
+  return <div>ItemSecondary</div>;
+};
---
+++ data/dataBannerHome.js
@@ -0,0 +1,30 @@
+export const DataBannerHome = [
+  {
+    tittle: "Explora tus mejores vacaciones",
+    subtittle: "planifica tu viajes con nosotros",
+    img: "/imagenes/home/banner.jpg",
+    principa: true,
+    description: "pequema descripción",
+  },
+  {
+    tittle: "Explora tus mejores vacaciones1",
+    subtittle: "planifica tu viajes con nosotros1",
+    img: "",
+    principa: false,
+    description: "",
+  },
+  {
+    tittle: "Explora tus mejores vacaciones2",
+    subtittle: "planifica tu viajes con nosotros2",
+    img: "",
+    principa: false,
+    description: "",
+  },
+  {
+    tittle: "Explora tus mejores vacaciones3",
+    subtittle: "planifica tu viajes con nosotros3",
+    img: "",
+    principa: false,
+    description: "",
+  },
+];
