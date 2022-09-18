d3b96a53d6ef8493946d9ba3b5a8e1da659198ff
jeanflores99 <ajiliof@gmail.com>
2022-09-17 20:31:33 -0500

    loader

--- components/loader/index.js
+++ components/loader/index.js
@@ -5,10 +5,9 @@ display: block;
 #globalLoader{
     position: fixed;
     z-index: 1700;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #fff;
+    top: 0;
+    left: 0;
+    background-color: #F0F2F5;
     display: flex;
     left: 0,
     right: 0;
@@ -17,42 +16,11 @@ display: block;
     justify-content: center;
     align-items: center;
 }
-.loader {
- --clr: #3498db;
- /* color of spining  */
- width: 50px;
- height: 50px;
- position: relative;
+
+#svgavion {
+ width: 100px;
+ height: 100px;
+
 }
-.loader div:nth-child(1), .loader div:nth-child(2) {
- content: "";
- position: absolute;
- top: -10px;
- left: -10px;
- width: 100%;
- height: 100%;
- border-radius: 100%;
- border: 10px solid transparent;
- border-top-color: var(--clr);
-}
-.loader div:nth-child(1) {
- z-index: 100;
- animation: spin 1s infinite;
-}
-.loader div:nth-child(2) {
- border: 10px solid #ccc;
-}
-@keyframes spin {
- 0% {
-  -webkit-transform: rotate(0deg);
-  -ms-transform: rotate(0deg);
-  -o-transform: rotate(0deg);
-  transform: rotate(0deg);
- }
- 100% {
-  -webkit-transform: rotate(360deg);
-  -ms-transform: rotate(360deg);
-  -o-transform: rotate(360deg);
-  transform: rotate(360deg);
- }
-}`;
+
+`;
