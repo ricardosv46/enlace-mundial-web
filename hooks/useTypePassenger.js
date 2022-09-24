9e56e4cfa42d356ce2ef77069bf858541f11a37f
jeanflores99 <ajiliof@gmail.com>
2022-09-24 10:18:46 -0500

    optimizando useTypePassenger, agregando el new array fill

--- hooks/useTypePassenger.js
+++ hooks/useTypePassenger.js
@@ -16,26 +16,12 @@ export const useTypePassenger = () => {
     // Asignar formularios
     useEffect(() => {
         const carritoLocal = JSON.parse(localStorage.getItem('carrito'))
-
-        // Calcular cuantos formularios se renderizarán
-        let forms = []
-        for (let i = 0; i < carritoLocal.nroAdultos; i++) {
-            let form = {
-                ...datosFormulario,
-                tipoPersona: 'ADULTO'
-            }
-            forms.push(form)
-        }
-        for (let i = 0; i < carritoLocal.nroMenores; i++) {
-            let form = {
-                ...datosFormulario,
-                tipoPersona: 'MENOR'
-            }
-
-            forms.push(form)
-        }
-
-        setDatePassenger(forms)
+        
+        {/* creando array con la cantidad de nro de adultos y nro de menores*/}
+        {/* los numeros de personas estan en el local storage*/}
+        let formMayores = new Array(carritoLocal.nroAdultos).fill({ ...datosFormulario, tipoPersona: 'ADULTO' })
+        let formMenores = new Array(carritoLocal.nroMenores).fill({ ...datosFormulario, tipoPersona: 'MENOR' })
+        setDatePassenger([...formMayores, ...formMenores])
     }, [])
 
     return {
