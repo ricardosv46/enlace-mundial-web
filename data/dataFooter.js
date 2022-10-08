fc4f7552dd0a43ec7c708fb625418ba50ce198bb
jeanflores99 <ajiliof@gmail.com>
2022-10-08 12:42:51 -0500

    agregando el data footer.js

---
+++ data/dataFooter.js
@@ -0,0 +1,32 @@
+export const dataFooter = [
+    {
+        tittle: 'Luna de Miel',
+        items: [{
+            subtittle: 'San Valentín',
+            pathname: ''
+        }, {
+            subtittle: 'Viajes Familiares',
+            pathname: '',
+        }, {
+            subtittle: 'Recreación',
+            pathname: '',
+        }]
+    },
+    {
+        tittle: 'Cruceros',
+        items: [{
+            subtittle: 'Recreación',
+            pathname: ''
+        }, {
+            subtittle: 'Viajes Familiares',
+            pathname: '',
+        }, {
+            subtittle: 'Fin de Semana en Familia',
+            pathname: '',
+        },
+        {
+            subtittle: 'San Valentín',
+            pathname: ''
+        },]
+    }
+]
