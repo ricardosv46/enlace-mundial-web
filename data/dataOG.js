eea157ab8053da4cd8cfe56f551428714621f96c
RICARDO SOLIS <ricardosv46@gmail.com>
2022-11-14 22:44:33 -0500

    Feat: se agrega logo con otra resolucion

--- data/dataOG.js
+++ data/dataOG.js
@@ -1,6 +1,6 @@
 export const dataOG = {
   tittle: 'Enlace Mundial - Perú',
   description: 'Somos una agencia de viajes con 12 años en el servicio turístico y contamos con un excelente staff de profesionales del área. Convertimos tu viaje en una experiencia mundial.',
-  logo: `${process.env.SITE_URL}/logo.webp`,
+  logo: `${process.env.SITE_URL}/logo-enlace.png`,
   url: `${process.env.SITE_URL}`
 }
